f4b652d698c3d2a90be2845a951b0c78ecaf18b1
Yacine MAOUCHE <[yacine92maouche@gmail.com]>
2022-11-23 13:36:16 +0100

    screen

---
+++ components/Screen/index.jsx
@@ -0,0 +1,11 @@
+import './Screen.module.css'
+
+const Screen = function () {
+    return (
+        <div className='h-screen w-full flex items-center px-10'>
+            <h1 className='text-gray-300 text-xl sm:text-4xl'>Salut, je suis Yacine MAOUCHE</h1>
+        </div>
+    )
+}
+
+export default Screen
---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import styles from '../styles/Home.module.css'
+
+import Screen from '../components/Screen'
+
+export default function Home() {
+  return (
+    <div>
+      <Screen />
+    </div>
+  )
+}
