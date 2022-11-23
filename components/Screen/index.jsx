513a2ff1f33d400ff211d0e528b27216a31b6eb1
Yacine MAOUCHE <[yacine92maouche@gmail.com]>
2022-11-24 00:34:21 +0100

    test Screen

--- components/Screen/index.jsx
+++ components/Screen/index.jsx
@@ -1,9 +1,40 @@
-import './Screen.module.css'
+import styles from './Screen.module.css'
+import Typewriter from 'typewriter-effect';
+import React from "react"
 
 const Screen = function () {
     return (
-        <div className='h-screen w-full flex items-center px-10'>
-            <h1 className='text-gray-300 text-xl sm:text-4xl'>Salut, je suis Yacine MAOUCHE</h1>
+        <div className={styles.screen}>
+            <h1>
+                <Typewriter
+                    options={{
+                        autoStart: true,
+                        deleteSpeed: 20,
+                        delay: 75,
+                    }}
+                    onInit={(typewriter) => {
+                        typewriter.typeString('Salut !<br/>')
+                        .pauseFor(1000)
+                        //.deleteChars(7)
+                        .typeString("Yacine MAOUCHE<br/>")
+                        .pauseFor(1000)
+                        .typeString("Dev Web Full Stack<br/>")
+                        .pauseFor(1000)
+                        //.deleteChars(14)
+                        .typeString("Css")
+                        .pauseFor(1000)
+                        //.deleteChars(3)
+                        .typeString(", Javascript")
+                        .pauseFor(1000)
+                        //.deleteChars(10)
+                        .typeString(", PHP")
+                        .pauseFor(1000)
+                        //.deleteChars(3)
+                        .typeString(", React")
+                        .start();
+                    }}
+                />
+            </h1>
         </div>
     )
 }
